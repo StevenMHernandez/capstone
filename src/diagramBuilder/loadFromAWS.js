3b50f6a61ad481d954e3cfa5b94dd7cc20411a1a
vadrevua <vadrevua@vcu.edu>
2017-12-24 14:20:24 -0500

    Added a way to save information from aws. EC2 name, Loadbalancer Name, RDB & EC2 Security groups printed to console

--- src/diagramBuilder/loadFromAWS.js
+++ src/diagramBuilder/loadFromAWS.js
@@ -18,21 +18,42 @@ module.exports = function (region, tag, requestedResource, callback) {
     var ec2 = new AWS.EC2();
     var elb = new AWS.ELB();
     var rds = new AWS.RDS();
+    var ec2KeyData = "Name of EC2 Server with ASV tags ";
+    var elbTags = "\n";
+    var ec2SecurityGroups = "";
+    var dbSecurityGroups = "";
+
 
     switch (requestedResource) {
         case ("EC2"):
             ec2.describeTags(params, function (err, data) {
                 callback(data);
+                for(i=1; i<data.Tags.length; i++){
+                    if (data.Tags[i].Key == "ASV") {
+                        ec2KeyData += data.Tags[i].Value + " ";
+                    }
+                }
+                console.log(ec2KeyData);
             });
             break;
         case("ELB"):
             elb.describeTags(params, function (err4, data) {
                 callback(data);
+                for(i=1; i<data.TagDescriptions.length; i++) {
+                    elbTags += data.TagDescriptions[i].LoadBalancerName + " ";
+                }
+                console.log(elbTags);
             });
             break;
         case("RDS"):
             rds.describeDBSecurityGroups(params, function (err5, data) {
                 callback(data);
+                for(i=1; i<data.DBSecurityGroups.length; i++) {
+                    ec2SecurityGroups = data.DBSecurityGroups[i].EC2SecurityGroups + " ";
+                    dbSecurityGroups = data.DBSecurityGroups[i].DBSecurityGroupName + " ";
+                }
+                console.log(ec2SecurityGroups);
+                console.log(dbSecurityGroups);
             });
             break;
         default:
@@ -40,6 +61,8 @@ module.exports = function (region, tag, requestedResource, callback) {
                 callback(data);
             });
     }
+
+
 };
 
 
