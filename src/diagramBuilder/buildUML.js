fcd54aaf1dccd0433038c732e0220252e51ef124
StevenMHernandez <contact@stevenhdesigns.com>
2018-01-06 13:40:50 -0500

    * added simple plantuml file, this file will be converted into a `.mustache` file to load generate the correct .puml * added test to ensure that `buildUML` creates a file as expected * stubbed out code that needs to be added to the `buildUML` function

--- src/diagramBuilder/buildUML.js
+++ src/diagramBuilder/buildUML.js
@@ -7,8 +7,20 @@
  * or files to handle the each different element
  * depending on how we structure the code.
  *
+ * @param filename
  * @param data
  * @returns {*}
  */
 module.exports = function (filename, data) {
+    // TODO: we need to find a way to load this file as a string.
+    // load_file is not the correct function.
+    let mustacheString = load_file(__dirname + '../../../resources/mustache/AWS_CLOUD.mustache');
+
+    // TODO: We need to install https://github.com/janl/mustache.js/
+    // nodejs usage see: https://stackoverflow.com/a/8106610
+    let renderpumlString = Mustache.render(mustacheString, data);
+
+    // TODO: We then need to save the string to a file at the file location specified in filename
+    // file_save is not the correct function.
+    file_save(filename, renderpumlString);
 };
---
+++ test/testBuildUML.js
@@ -0,0 +1,81 @@
+let chai = require('chai');
+let assert = chai.assert;
+let expect = chai.expect;
+let buildUML = require('../src/diagramBuilder/buildUML');
+
+chai.use(require('chai-fs'));
+
+let region = 'us-east-1';
+let tag = 'PROD_SERVER';
+
+describe('testBuildUML()', function () {
+    describe('EC2', function () {
+        it('should create a puml file in the location defined', function () {
+
+
+            let object = {
+                "subnets": [
+                    {
+                        ec2: [
+                            {
+                                'id': 'api1',
+                                'name': 'Windows2016',
+                            },
+                            {
+                                'id': 'api2',
+                                'name': 'AmazonLinux',
+                            },
+                            {
+                                'id': 'api3',
+                                'name': 'RedHatService',
+                            }
+                        ],
+                        rds: [
+                            {
+                                'id': 'db1',
+                                'name': 'MySQL1',
+                            },
+                            {
+                                'id': 'db2',
+                                'name': 'MySQL1',
+                            }
+                        ],
+                    },
+                    {
+                        ec2: [],
+                        rds: [
+                            {
+                                'id': 'db3',
+                                'name': 'MySQL3',
+                            },
+                            {
+                                'id': 'db4',
+                                'name': 'MySQL4',
+                            }
+                        ],
+                    },
+                    {
+                        ec2: [
+                            {
+                                'id': 'api5',
+                                'name': 'RedHatService',
+                            }
+                        ],
+                        rds: [
+                            {
+                                'id': 'db5',
+                                'name': 'MySQL5',
+                            }
+                        ],
+                    },
+                ]
+            };
+
+            buildUML("~/.storage/ec2_file.puml", object);
+
+            expect("~/.storage/ec2_file.puml").to.be.a.file();
+
+
+        });
+    });
+});
