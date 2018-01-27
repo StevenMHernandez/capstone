43088e1ca3aaf73cc1e8f8895e18b2671fa9b05a
StevenMHernandez <contact@stevenhdesigns.com>
2018-01-26 22:01:41 -0500

    * run plantuml in nodejs script * test file outputs

---
+++ test/testPumlToPNG.js
@@ -0,0 +1,31 @@
+let chai = require('chai');
+let assert = chai.assert;
+let expect = chai.expect;
+let pumlToPNG = require('../src/diagramBuilder/pumlToPNG');
+var fs = require('fs');
+
+chai.use(require('chai-fs'));
+
+const outputFile = __dirname + "/.storage/diagram.png";
+
+describe('testPumlToPNG()', function () {
+    this.timeout(10000);
+
+    before(function () {
+        if (fs.existsSync(outputFile)) {
+            fs.unlinkSync(outputFile);
+        }
+    });
+
+    it('should create a png file in the location defined', function (done, x) {
+         pumlToPNG(__dirname + "/.storage/diagram.puml")
+            .then(function () {
+                expect(outputFile).to.be.a.file();
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+        //
+    });
+});
